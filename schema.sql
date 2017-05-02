2d3ae4cbf63528e3039e0670c901843b70301c02
skywind29 <skywind29@users.noreply.github.com>
2017-05-02 21:51:39 +0800

    schema update

--- schema.sql
+++ schema.sql
@@ -1,6 +1,13 @@
-CREATE TABLE IF NOT EXISTS records 
+CREATE TABLE IF NOT EXISTS gps_records 
 (db_id 		INTEGER 					PRIMARY KEY, 
-device_id 	INTEGER 		NOT NULL, 
+device_id 	TEXT			NOT NULL, 
 lng 		DECIMAL(9, 6) 	NOT NULL, 
 lat 		DECIMAL(9, 6) 	NOT NULL, 
+saved_time 	TIMESTAMP		NOT NULL  	DEFAULT CURRENT_TIMESTAMP);
+
+CREATE TABLE IF NOT EXISTS phonecall_records 
+(db_id 		INTEGER 					PRIMARY KEY, 
+device_id 	TEXT 			NOT NULL, 
+phone_num 	TEXT		 	NOT NULL, 
+called_time TIMESTAMP 		NOT NULL, 
 saved_time 	TIMESTAMP		NOT NULL  	DEFAULT CURRENT_TIMESTAMP);
