4259d67e20914efb76c65d00404390be985dee87
skywind29 <skywind29@users.noreply.github.com>
2017-05-06 05:53:04 +0800

    adjust form of headers

--- const.py
+++ const.py
@@ -3,4 +3,4 @@ SMS_USERNAME = "簡訊王帳號"
 SMS_PASSWORD = "簡訊王密碼"
 HOST = "127.0.0.1"	#Set this to '0.0.0.0' to have the server available externally.
 PORT = 5000
-headers = 'Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept, Connection, User-Agent, Cookie'
+headers = ['Access-Control-Allow-Headers: Origin', 'X-Requested-With', 'Content-Type, Accept', 'Connection', 'User-Agent', 'Cookie']
